16839f4168fe68da6424a1cc3940fcc1b182773a
PKrokosz <98399042+PKrokosz@users.noreply.github.com>
2025-11-05 00:15:53 +0100

    fix: ignore dist in eslint

--- eslint.config.js
+++ eslint.config.js
@@ -3,12 +3,14 @@ import tseslint from "typescript-eslint";
 import prettier from "eslint-config-prettier";
 
 export default tseslint.config(
+  {
+    ignores: ["dist/**"]
+  },
   js.configs.recommended,
   ...tseslint.configs.recommended,
   prettier,
   {
     files: ["**/*.ts"],
-    ignores: ["dist/**"],
     rules: {
       "no-console": ["warn", { allow: ["info", "warn", "error"] }]
     }
